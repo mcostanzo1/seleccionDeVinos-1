7a59cc1f10ea458a8b63d264db860513e4a90677
Martín Depetris <depetris.martin@gmail.com>
2017-07-08 13:09:04 -0300

    Inicio de proyecto

---
+++ src/main/java/com/depetris/sdv/SelecciónDeVinosApplication.java
@@ -0,0 +1,12 @@
+package com.depetris.sdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecciónDeVinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecciónDeVinosApplication.class, args);
+	}
+}
