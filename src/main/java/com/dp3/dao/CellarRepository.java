2db07f12ee518292a96a394d275354a386cd21da
lab-admin <mariano.a.costanzo@gmail.com>
2018-01-18 14:41:11 -0300

    UI changes commit

--- src/main/java/com/dp3/dao/CellarRepository.java
+++ src/main/java/com/dp3/dao/CellarRepository.java
@@ -3,7 +3,6 @@ package com.dp3.dao;
 import com.dp3.domain.Cellar;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import sun.jvm.hotspot.jdi.IntegerTypeImpl;
 
 @Repository
 public interface CellarRepository extends MongoRepository<Cellar, String> {
--- src/main/java/com/dp3/dao/StockRepository.java
+++ src/main/java/com/dp3/dao/StockRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dp3.domain.Stock;
-import sun.jvm.hotspot.jdi.IntegerTypeImpl;
 
 @Repository
 public interface StockRepository extends MongoRepository<Stock, String> {
